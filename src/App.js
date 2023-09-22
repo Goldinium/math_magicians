df9e6b891be98171aa1741e361bd9e91fe862b9a
Badr BenSlama <badrbenslama8@gmail.com>
2023-09-22 13:10:33 +0100

    fetching functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './index.css';
 import Calculator from './components/Calculator';
+import Quote from './components/fetchQuotes';
 
 export default function App() {
   return (
     <>
       <Calculator />
+      <Quote />
       <span>im a text</span>
     </>
   );
